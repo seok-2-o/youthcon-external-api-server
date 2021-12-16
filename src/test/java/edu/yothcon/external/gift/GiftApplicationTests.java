968741d371a7456a80a51e0aa40534d23ba89ade
seok <seogi89@gmail.com>
2021-12-16 12:47:34 +0900

    Initial commit

---
+++ src/test/java/edu/yothcon/external/gift/GiftApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.yothcon.external.gift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
